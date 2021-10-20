7d28d0ac30f558147fd9fcbad5e882448395a79e
Akib3502 <92708769+Akib3502@users.noreply.github.com>
2021-10-20 12:24:37 +0530

    Add files via upload

---
+++ mark3.c
@@ -0,0 +1,14 @@
+/* variable Declaration*/
+#include<stdio.h>
+int main()
+{
+	int roll;
+	float mark;
+	char grade;
+	roll=56;
+	mark=86;
+	grade='A';
+	printf(" roll number :%d\n",roll);
+	printf(" mark obtain :%f\n",mark);
+	printf(" grade:%c\n",grade);
+}
