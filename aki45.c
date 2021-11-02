af2ba716298d874e320b7591fd87990ccaf67607
Akib3502 <92708769+Akib3502@users.noreply.github.com>
2021-11-02 11:42:29 +0530

    Add files via upload

---
+++ aki45.c
@@ -0,0 +1,12 @@
+/*  all arithmatical problem */
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	a=56;
+	b=45;
+	c=6;
+	printf("sum of three numbers is %d:",a+b+c);
+	printf("\n product of three number is %d:",a*b*c);
+	printf("\n average of three number is %d:",(a+b+c)/3);
+}
