7f14cbd777683142d0b234c7786baa9496a9136e
Akib3502 <92708769+Akib3502@users.noreply.github.com>
2021-11-08 12:04:37 +0530

    Add files via upload

---
+++ akib78.c
@@ -0,0 +1,14 @@
+/* Relational or comparison operation*/
+#include<stdio.h>
+void main()
+{
+	int a,b;
+	a=20;
+	b=10;
+	printf("%d>%d...%d",a,b,a>b);
+	printf("\n%d>=%d...%d",a,b,a>=b);
+	printf("%\n%d<%d...%d",a,b,a<b);
+	printf("\n%d<=%d...%d",a,b,a<=b);
+	printf("\n%d==%d...%d",a,b,a==b);
+	printf("\n%d!=%d...%d",a,b,a!=b);
+}
